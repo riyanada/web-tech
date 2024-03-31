966e009cee41a2238fb3478499e4f4695c6318fa
riyanada <riyanmaulana402@yahoo.co.id>
2024-03-31 08:59:21 +0700

    tugas-4

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 
 // Relasi 1 - one to one
-Route::get('relasi-1', function() {
+Route::get('relasi-1', function () {
     // Temukan mahasiswa
     $mahasiswa = App\Models\Mahasiswa::where('nim', '=', 'D015015072')->first();
 
@@ -28,18 +28,44 @@ Route::get('relasi-1', function() {
 });
 
 // Relasi one to Many
-Route::get('relasi-2', function() {
+Route::get('relasi-2', function () {
     $mahasiswa = App\Models\Mahasiswa::where('nim', '=', 'D015015072')->first();
     return $mahasiswa->dosen->nama;
 });
 
 Route::get('relasi-3', function () {
     $dosen = App\Models\Dosen::where('nama', '=', 'Eko')->first();
-    foreach ($dosen->mahasiswa as $temp) {  
+    foreach ($dosen->mahasiswa as $temp) {
         echo '<li style="margin-bottom: 10px;">';
         echo '<span style="font-weight: bold;">Nama: </span>' . $temp->nama . '<br>';
         echo '<span style="font-weight: bold;">NIM: </span>' . $temp->nim;
         echo '</li>';
-
     }
+});
+
+// Relasi Many to Many
+Route::get('relasi-4', function () {
+    $ani = App\Models\Mahasiswa::where('nama', '=', 'Ani')->first();
+    foreach ($ani->hobi as $temp) {
+        echo '<li>' . $temp->hobi . '</li>';
+    }
+});
+
+
+Route::get('relasi-5', function () {
+    $menulis = App\Models\Hobi::where('hobi', '=', 'Menulis')->first();
+
+    echo '<h2>Mahasiswa yang Menulis:</h2>';
+    echo '<ul>';
+
+    foreach ($menulis->mahasiswa as $temp) {
+        echo '<li>Nama: ' . $temp->nama . ' - NIM: <strong>' . $temp->nim . '</strong></li>';
+    }
+
+    echo '</ul>';
+});
+
+Route::get('/mahasiswa', function () {
+    $mahasiswas = App\Models\Mahasiswa::with('hobi', 'wali', 'dosen')->get();
+    return view('mahasiswa', compact('mahasiswas'));
 });
